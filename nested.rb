a73b32d69a03fcd1f333f3562d3a511eed465a8c
Steven Hunal <stevehunal@gmail.com>
2019-07-30 18:22:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     string = programmer_hash[:dennis_ritchie][:language]
+     string = programmer_hash[:dennis_ritchie][:languages]
      puts string
 end
 
